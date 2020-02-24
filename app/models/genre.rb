4b038e1b708998dd280da146b0fdaaa3771a5d27
takfujii29 <takfujii29@icloud.com>
2020-02-24 20:14:47 +0900

    add-rspc

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ActiveHash::Base
   self.data = [
-      {id: 1, name: 'HTML'}, {id: 2, name: 'CSS'}, {id: 3, name: 'Ruby'}, {id: 4, name: 'JavaScript'}, {id: 5, name: 'linux'}, {id: 6, name: 'その他'}
+      {id: 1, name: 'HTML'}, {id: 2, name: 'CSS'}, {id: 3, name: 'Ruby'}, {id: 4, name: 'JavaScript'}, {id: 5, name: 'linux'}, {id: 6, name: 'RSpec'}, {id: 7, name: 'その他'}
   ]
 end
