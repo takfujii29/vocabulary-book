d9692f968a3b73a6272370c7adcd7926dc58df51
takfujii29 <takfujii29@icloud.com>
2020-03-04 16:44:38 +0900

    edit-index

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ActiveHash::Base
   self.data = [
-      {id: 1, name: 'HTML'}, {id: 2, name: 'CSS'}, {id: 3, name: 'Ruby'}, {id: 4, name: 'JavaScript'}, {id: 5, name: 'linux'}, {id: 6, name: 'RSpec'}, {id: 7, name: 'C++'}, {id: 8, name: 'その他'}
+      {id: 1, name: 'HTML'}, {id: 2, name: 'CSS'}, {id: 3, name: 'Ruby'}, {id: 4, name: 'JavaScript'}, {id: 5, name: 'linux'}, {id: 6, name: 'RSpec'}, {id: 7, name: 'C++'}, {id: 8, name: 'PHP'}, {id: 9, name: 'java'}, {id: 10, name: 'Python'}, {id: 11, name: 'その他'}
   ]
 end
